febf67aa6edfafbf911e474b95e0c71a86eab8c8
Gom <gomgomgom@gmail.com>
2014-09-28 00:32:43 +0100

    commiting some changes to main branch ahead of big refactor

--- spiderBro.py
+++ spiderBro.py
@@ -13,7 +13,8 @@ from sb_utils import *
 startTime = datetime.now()
 e_masks = [sNeN, NxN, NNN]
 s_masks = [season, series]
-ignore_taglist = ["SWESUB", "SPANISH", "GERMAN", "WEBRIP", "HBOGO"]
+ignore_taglist = ["SWESUB", "SPANISH", "GERMAN", "HBOGO"]
+#ignore_taglist = ["SWESUB", "SPANISH", "GERMAN", "WEBRIP", "HBOGO"]
 #search_list = [piratebaysearch, isohuntsearch, extratorrentsearch]
 search_list = [piratebaysearch]
 socket.setdefaulttimeout(10)
@@ -39,32 +40,30 @@ shows_list = [val for val in shows_list if val not in ignore_list]
 # Main
 #################################################################################################################
 if(opts.all):
-    #
     # if ALL, we get the complete list of shows from xbmc, minus the finished shows (if any)
-    #
     log.info("Scanning entire XBMC library, this could take some time...")
-
-    full_showlist = db.xbmc_get_showlist()
+    full_showlist = []
+    db_showlist = db.xbmc_get_showlist()
+    for show in db_showlist:
+        if(show[0].decode('latin-1', 'replace') not in ignore_list): full_showlist.append(show[0])
+    #TODO: add this to config file
+    get_trakt_watch_list = True
+    if(get_trakt_watch_list):
+        log.info("Looking for shows from trakt.com watchlist")
+        # get the watchlist from trakt and add
+        traktlist = traktWatchlistScraper("thegom145", "b837e9f111dcae8e279711ce929e9ef1")
+        full_showlist.extend(t for t in traktlist if t.decode('latin-1', 'replace') not in ignore_list)
+        full_showlist.sort()
     for show in full_showlist:
-        if(show[0].decode('latin-1', 'replace') not in ignore_list):
-            hunt_eps(show[0], opts, search_list, s_masks, e_masks, ignore_taglist)
+        if(show.decode('latin-1', 'replace') not in ignore_list):
+            hunt_eps(show, opts, search_list, s_masks, e_masks, ignore_taglist)
 
 else:
-    #
     # if SHOW, get specified show
-    #
     if(opts.show):
         hunt_eps(opts.show, opts, search_list, s_masks, e_masks, ignore_taglist)
-    #
-    # if SHOWS_LIST, get for list of shows
-    #
-    elif(shows_list):
-        log.info("Using list of shows from file...")
-        for show in shows_list:
-            hunt_eps(show, opts, search_list, s_masks, e_masks, ignore_taglist)
-    #
+    
     # else EXIT
-    #
     else:
         log.info("No shows to download, exiting")
         sys.exit()
